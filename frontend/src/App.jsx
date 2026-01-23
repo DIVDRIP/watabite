3b23529600d081460da0d7d41a1252ba035cfd58
Divyanshu <divyanshuworldwide@gmail.com>
2026-01-23 16:51:36 +0530

    Add basic files install react router domand toast and make navigation menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Navbar from "./components/Navbar";
 
-function App() {
-
+const App = () => {
+  // const isSellerPath = useLocation().pathname.includes("seller");
   return (
-   <div>
-    <h1 className='text-gray-700 text-[30px] bg-green-400'>hi</h1>
-   </div>
+    <div className="bg-black text-white">
+      <Navbar />
+
+
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
